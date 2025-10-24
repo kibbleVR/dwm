dab5d060b3019d10f8d137a5c8ddb2da652eea88
kibble <kibble@thinkpad.phub.net.cable.rogers.com>
2025-10-24 07:01:33 -0400

    i use void btw

--- config.h
+++ config.h
@@ -1,25 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=18" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
+static const char col_gray1[]       = "#303446";
+static const char col_gray2[]       = "#414559";
+static const char col_gray3[]       = "#c6d0f5";
+static const char col_gray4[]       = "#626880";
+static const char col_cyan[]        = "#8caaee";
+static const char col_lavender[]    = "#babbf1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_lavender, col_cyan,  col_cyan  },
 };
 
 /* tagging */
-static const char *tags[] = { "", "󰈹", "󰕼", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "󰈹", "󰕼", "", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,7 +29,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "st",       NULL,       NULL,       1,            0,           -1 },
-	{ "Firefox",  NULL,       NULL,       2,            0,           -1 },
+	{ "qutebrowser",  NULL,       NULL,       2,            0,           -1 },
+	{ "discordo",  NULL,       NULL,      4,            0,           -1 },
 };
 
 /* layout(s) */
@@ -60,14 +62,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *rofi[] = {"rofi", "-show", "drun"};
+static const char *qutecmd[]  = { "qutebrowser", NULL };
+//static const char *rofi[] = {"rofi", "-show", "drun"};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_d,      spawn,          {.v = rofi } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = qutecmd } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_u,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
